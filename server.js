789bd1d1c742272b210da343b5b7cc5b7be3ecd7
suksham <129354626+suksham11@users.noreply.github.com>
2024-09-20 18:43:45 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  console.log("New user connected");
+
+  socket.on("newuser", (username) => {
+    console.log(`${username} has joined`);
+    socket.broadcast.emit("update", `${username} joined the chat`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
